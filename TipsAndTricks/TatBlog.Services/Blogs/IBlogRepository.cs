792573b9298387f1bc6170953b90acbd8402b4a3
bui van du <buivanducd2002@gmail.com>
2023-02-28 21:40:14 +0700

    fifth commit

---
+++ TipsAndTricks/TatBlog.Services/Blogs/IBlogRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.DTO;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs
+{
+    public interface IBlogRepository
+    {
+        Task<Post> GetPostAsync(
+            int year,
+            int month,
+            string slug,
+            CancellationToken cancellationToken = default);
+
+        Task<IList<Post>> GetPopularArticlesAsync(
+            int numPosts,
+            CancellationToken cancellationToken= default);
+
+        Task<bool> IsPostSlugExixtedAsync(
+            int postID, string slug,
+            CancellationToken cancellationToken=default);
+
+        Task IncreaseViewCountAsync(
+            int postId,
+            CancellationToken cancellationToken = default);
+            
+        Task<IList<CategoryItem>> GetCategoriesAsync(
+            bool showOnMenu=false,
+            CancellationToken cancellationToken=default);
+           
+    }
+}
--- TipsAndTricks/TatBlog.WinApp/Program.cs
+++ TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,20 +1,89 @@
-﻿using TatBlog.Data.Contexts;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
+
+
+
+//var seeder=new DataSeeder(context);
+
+//seeder.Initialize();
+
+//var authors = context.Author.ToList();
+
+//Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}",
+//    "ID", "Full Name", "Email", "Joined Date");
+
+//foreach (var author in authors)
+//{
+//    Console.WriteLine(  "{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
+//        author.Id, author.FullName, author.Email, author.JoinedDate);
+//}
+
+
+
+
+
+
+//var context = new BlogDbContext();
+//var posts = context.Posts
+//    .Where (x => x.Published)
+//    .OrderBy (x => x.Title)
+//    .Select(p => new
+//    {
+//        Id=p.Id,
+//        Title =p.Title,
+//        ViewCount = p.ViewCount,
+//        PostedDate =p.PostedDate,
+//        Author =p.Author.FullName,
+//        Category =p.Category.Name,
+//    })
+//    .ToList ();
+
+//foreach (var post in posts)
+//{
+//    Console.WriteLine("ID       :{0}",post.Id);
+//    Console.WriteLine("Title    :{0}",post.Title);
+//    Console.WriteLine("View     :{0}",post.ViewCount);
+//    Console.WriteLine("Date     :{0}:MM/dd/yyyy", post.PostedDate);
+//    Console.WriteLine("Author   :{0}", post.Author);
+//    Console.WriteLine("Category :{0}", post.Category);
+//    Console.WriteLine("".PadRight(80,'-'));
+//}
+
+
+
+//var context = new BlogDbContext();
+
+//IBlogRepository blogRepo = new BlogRepository(context);
+
+//var posts = await blogRepo.GetPopularArticlesAsync(3);
+
+//foreach (var post in posts)
+//{
+//    Console.WriteLine("ID       :{0}",post.Id);
+//    Console.WriteLine("Title    :{0}",post.Title);
+//    Console.WriteLine("View     :{0}",post.ViewCount);
+//    Console.WriteLine("Date     :{0}:MM/dd/yyyy", post.PostedDate);
+//    Console.WriteLine("Author   :{0}", post.Author.FullName);
+//    Console.WriteLine("Category :{0}", post.Category.Name);
+//    Console.WriteLine("".PadRight(80,'-'));
+//}
 
 
 var context = new BlogDbContext();
 
-var seeder=new DataSeeder(context);
+IBlogRepository blogRepo = new BlogRepository(context);
 
-seeder.Initialize();
+var categories = await blogRepo.GetCategoriesAsync();
 
-var authors = context.Author.ToList();
+Console.WriteLine("{0,-5}{1,-50}{2,10}",
+    "ID", "Name", "Count");
 
-Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}",
-    "ID", "Full Name", "Email", "Joined Date");
-
-foreach (var author in authors)
+foreach (var item in categories)
 {
-    Console.WriteLine(  "{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
-        author.Id, author.FullName, author.Email, author.JoinedDate);
+    Console.WriteLine("{0,-5}{1,-50}{2,10}",
+        item.Id, item.Name, item.PostCount);
 }
