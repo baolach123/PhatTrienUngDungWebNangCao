407ba56590917f4bd5a22cd9ae22e8f7bfbb54c9
bui van du <buivanducd2002@gmail.com>
2023-03-08 14:33:57 +0700

    lab02

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            return View();
+        }
+
+        public IActionResult About()        
+            =>View();
+
+        public IActionResult Contact()
+            => View();
+
+        public IActionResult Rss()
+            => Content("Noi dung se duoc cap nhat");
+        
+    }
+}
